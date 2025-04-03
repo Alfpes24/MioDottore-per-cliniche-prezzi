4c0c8b7eaf657251cfd0820a9ad8ac8d0eb5cb8c
Alfpes24 <apesce.consulente@gmail.com>
2025-04-03 13:02:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -88,7 +88,7 @@ document.addEventListener("DOMContentLoaded", () => {
         updateViewerCount();
         setInterval(updateViewerCount, 15000);
       }
-    }, 3000);
+    }, 20000);
   });
 
   // Messaggio cliccabile per scroll
